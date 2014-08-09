507c6df85076ea58d5535c369640a9227c0a0cfe
André König <andre.koenig@posteo.de>
2014-08-09 21:22:33 +0200

    Initial commit. Example from the article.

---
+++ README.md
@@ -0,0 +1,15 @@
+# example-verror
+
+This example refers to the [article](http://npmawesome.com/posts/2014-08-11-verror/) on [npmawesome.com](http://npmawesome.com) about `verror`.
+
+## Usage
+
+git clone https://github.com/npmawesome/example-verror.git
+cd example-verror
+npm install
+
+npm start
+
+## Source
+
+The [index.js]() contains the example from the article. When executing `npm start` you will see an expected error which demonstrates the object mapping of several messages.
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var fs = require('fs');
+var VError = require('verror');
+
+exports.readConfig = function readConfig (file, callback) {
+    function onRead (err, content) {
+        if (err) {
+            return callback(new VError(err, 'failed to read config file "%s"', file));
+        }
+
+        try {
+            content = JSON.parse(content);
+        } catch (e) {
+            return callback(new VError(e, 'failed to parse config file "%s"', file));
+        }
+
+        callback(null, content);
+    }
+
+    fs.readFile(file, onRead);
+};
+
+if (require.main === module) {
+    exports.readConfig('/not/available.json', function onRead (err, config) {
+        if (err) {
+            return console.error(err.message);
+        }
+        
+        console.log(config);
+    });
+}
